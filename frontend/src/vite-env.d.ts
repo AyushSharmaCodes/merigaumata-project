6e9c25c67840c9555b38f1dfb0fbfd4cec0f8fe8
Ayush Sharma <110625033+AyushSharmaCodes@users.noreply.github.com>
2026-04-04 21:32:02 +0530

    Update vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client"/>
 
 interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any 
     Razorpay: any;
 }
 
